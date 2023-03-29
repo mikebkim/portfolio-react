e3a6120640b13bb21a4b5c3ef8635b575f4ef2cf
Michael Kim <mikebkim@gmail.com>
2023-03-28 17:55:49 -0700

    updates

--- src/components/TechnologiesPage/Styled_TechnologiesPage.js
+++ src/components/TechnologiesPage/Styled_TechnologiesPage.js
@@ -8,8 +8,12 @@ const Styled_TechnologiesPage = styled.div`
     margin-right: auto;
     width: 95vw;
 
+    img {
+        height: auto;
+        max-width: 50%;
+    }
+
     .technologies-list {
-        margin-left: 10%;
         height: 90vh;
         width: 90%;
         display: flex;
@@ -18,10 +22,11 @@ const Styled_TechnologiesPage = styled.div`
         max-height: 80vh;
         margin-top: 10vh;
         align-items: center;
+        margin: 0 auto;
     }
 
     .flip-card {
-        background-color: transparent;
+        background-color: rgb(250 250 250);
         width: 100px;
         height: 100px;
         perspective: 1000px;
@@ -36,13 +41,18 @@ const Styled_TechnologiesPage = styled.div`
         width: 100%;
         height: 100%;
         text-align: center;
-        transition: transform .6s;
+        transition: transform 0.6s;
         transform-style: preserve-3d;
         border-radius: 5px;
-        box-shadow: 0 4px 8px 0 rgba(0,0,0,.2);
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
     }
 
-    .flip-card-back, .flip-card-front {
+    .flip-card:hover .flip-card-inner {
+        transform: rotateY(180deg);
+    }
+
+    .flip-card-front,
+    .flip-card-back {
         display: flex;
         justify-content: center;
         align-items: center;
@@ -54,17 +64,15 @@ const Styled_TechnologiesPage = styled.div`
         backface-visibility: hidden;
     }
 
-    /* .flip-card-back {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 5px;
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        -webkit-backface-visibility: hidden;
-        backface-visibility: hidden;
-    } */
+    .flip-card-front {
+        font-size: 6vh;
+    }
+
+    .flip-card-back {
+        background-color: black;
+        color: white;
+        transform: rotateY(180deg);
+    }
 }
 `
 
--- src/components/TechnologiesPage/TechnologiesPage.jsx
+++ src/components/TechnologiesPage/TechnologiesPage.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Styled_TechnologiesPage from "./Styled_TechnologiesPage"
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 const TechnologiesPage = ({ allImages }) => {
     const allImagesArray = Object.values(allImages)
+    useEffect(() => {
+        AOS.init()
+    }, [])
 
     return (
         <Styled_TechnologiesPage>
@@ -11,26 +16,30 @@ const TechnologiesPage = ({ allImages }) => {
                     <h1>Technologies</h1>
                     <p className="description">Below are languages, frameworks, tools, and ancillary technologies I provide when building
                         websites.
-                        <br />
                     </p>
-                    <br />
                 </div>
-                <h3>Development</h3>
                 <div className="technologies-list">
                     {allImagesArray.map(image => {
+                        const cssImage = image.includes("css") ? <img
+                            className="tech-image"
+                            width="35px"
+                            height="auto"
+                            src={image}
+                            key={image}
+                            alt={image}
+                        /> : <img
+                            className="tech-image"
+                            src={image}
+                            key={image}
+                            alt={image}
+                        />
+
                         return <div
-                            data-aos="fade-up"
                             className="flip-card aos-init aos-animate"
                         >
                             <div className="flip-card-inner">
                                 <div className="flip-card-front">
-                                    <img
-                                        src={image}
-                                        height="50"
-                                        width="50"
-                                        key={image}
-                                        alt={image}
-                                    />
+                                    {cssImage}
                                 </div>
                                 <div className="flip-card-back">
                                     <h3>Test</h3>
@@ -39,17 +48,6 @@ const TechnologiesPage = ({ allImages }) => {
                         </div>
                     })}
                 </div>
-                {/* <div className="technologies-list">
-                    {allImagesArray.map(image => {
-                        return <img
-                            src={image}
-                            height="50"
-                            width="50"
-                            key={image}
-                            alt={image}
-                        />
-                    })}
-                </div> */}
             </div>
         </Styled_TechnologiesPage>
     )

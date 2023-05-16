3e235682aa645c99776efc82c08efe79ce255f2b
Michael Kim <mikebkim@gmail.com>
2023-05-16 01:52:07 -0700

    Revert "Merge branch 'main' into master-mike"
    This reverts commit b8576f2f593c5857f0b0a18b11dc4e09072905a2, reversing
    changes made to 558af3ef797c49e493c134887b87a8269ab4807c.

---
+++ src/components/ProjectsPage/ProjectsPage.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect } from "react";
+import Styled_ProjectsPage from "./Styled_ProjectsPage";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const ProjectsPage = ({
+  iconBlackjack,
+  iconMarble,
+  iconSessions,
+  iconGameit,
+}) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <Styled_ProjectsPage>
+      <div className="ProjectsPage" data-aos="fade-up">
+        <div className="header">
+          <h1>PROJECTS</h1>
+          <hr />
+          <p className="description">
+            Below are my projects and technologies I used to create them.
+          </p>
+        </div>
+
+        <div className="projects-flex">
+          <a
+            className="project-link"
+            href="https://marblejerky.herokuapp.com/"
+            target="_blank"
+          >
+            <img
+              className="project-laptop"
+              src={iconMarble}
+              alt="marblejerky"
+            />
+          </a>
+          <div className="project">
+            <h2>Marble</h2>
+            <p className="project-description">
+              An eCommerce site that sells beef jerky. Check out the products!
+            </p>
+            <h3>Technologies Used</h3>
+            <div className="tech-list">
+              <p className="tech-name">HTML</p>
+              <p className="tech-name">CSS - Bootstrap</p>
+              <p className="tech-name">MERN Stack</p>
+              <p className="tech-name">MLab</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="projects-flex">
+          <a
+            className="project-link"
+            href="https://game-it.herokuapp.com/"
+            target="_blank"
+          >
+            <img className="project-laptop" src={iconGameit} alt="gameit" />
+          </a>
+          <div className="project">
+            <h2>game.it</h2>
+            <p className="project-description">
+              An app that allows users to compile, share, and chat about video
+              games.
+            </p>
+            <h3>Technologies Used</h3>
+            <div className="tech-list">
+              <p className="tech-name">HTML</p>
+              <p className="tech-name">CSS - Bulma</p>
+              <p className="tech-name">JavaScript</p>
+              <p className="tech-name">Node</p>
+              <p className="tech-name">Express</p>
+              <p className="tech-name">MongoDB</p>
+              <p className="tech-name">Socket.io</p>
+              <p className="tech-name">OAuth</p>
+              <p className="tech-name">IGDB API</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="projects-flex">
+          <a
+            className="project-link"
+            href="https://sessionsmusic.herokuapp.com/"
+            target="_blank"
+          >
+            <img
+              className="project-laptop"
+              src={iconSessions}
+              alt="sessionsmusic"
+            />
+          </a>
+          <div className="project">
+            <h2>Sessions</h2>
+            <p className="project-description">
+              A music tutor app that allows musicians to book sessions together.
+            </p>
+            <h3>Technologies Used</h3>
+            <div className="tech-list">
+              <p className="tech-name">HTML</p>
+              <p className="tech-name">CSS</p>
+              <p className="tech-name">Ruby on Rails</p>
+              <p className="tech-name">PostgreSQL</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="projects-flex">
+          <a
+            className="project-link"
+            href="https://mikebkim.github.io/blackjack/"
+            target="_blank"
+          >
+            <img
+              className="project-laptop"
+              src={iconBlackjack}
+              alt="blackjack"
+            />
+          </a>
+          <div className="project">
+            <h2>Blackjack</h2>
+            <p className="project-description">
+              A simple Blackjack game. You start off with $1,000. How much will
+              you win?
+            </p>
+            <h3>Technologies Used</h3>
+            <div className="tech-list">
+              <p className="tech-name">HTML</p>
+              <p className="tech-name">CSS</p>
+              <p className="tech-name">JavaScript</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Styled_ProjectsPage>
+  );
+};
+
+export default ProjectsPage;

ce57d439a806e23429fa49f13f413581d9b470f3
Mike Kim <mikebkim@gmail.com>
2026-03-10 11:54:21 -0700

    nothing important

--- src/components/FooterBar/Footer.Bar.js
+++ src/components/FooterBar/Footer.Bar.js
@@ -111,10 +111,11 @@ export const FooterWrap = styled.div`
       & > .social-icon {
         height: 3rem;
         width: 3rem;
-        margin: 0px 7rem 0px 2rem;
+        margin: 0 2rem;
         display: flex;
         justify-content: center;
         align-items: center;
+        margin-right: 7rem;
 
         & > .git {
           color: rgb(100, 100, 100);
